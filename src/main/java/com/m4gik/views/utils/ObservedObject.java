9b8a1402a24cf133014838347dd78d33cbc56e6d
m4gik <m4giksoftware@gmail.com>
2014-01-06 17:59:02 +0100

    Create class implements observer pattern

--- src/main/java/com/m4gik/views/utils/ObservedObject.java
+++ src/main/java/com/m4gik/views/utils/ObservedObject.java
@@ -8,11 +8,37 @@ package com.m4gik.views.utils;
 import java.util.Observable;
 
 /**
- * TODO COMMENTS MISSING!
+ * Class implements observer pattern.
  * 
  * @author m4gik <michal.szczygiel@wp.pl>
  * 
  */
 public class ObservedObject extends Observable {
 
+    private Boolean watchedValue;
+
+    /**
+     * Constructor of {@link ObservedObject}.
+     * 
+     * @param value
+     */
+    public ObservedObject(Boolean value) {
+        this.watchedValue = value;
+    }
+
+    /**
+     * This method adds the specified observer o to the set of observers for
+     * this object if, the observer is not the same as some observer already in
+     * the set.
+     * 
+     * @param value
+     */
+    public void setValue(Boolean value) {
+        // if value has changed notify observers
+        if (!watchedValue.equals(value)) {
+            watchedValue = value;
+        }
+        setChanged(); // mark as value changed
+        notifyObservers(value); // trigger notification
+    }
 }
